cd96d16cebafe2a47c9e8b4f77f3c3ecfdac3b67
John Ademoye <moyojohn188@gmail.com>
2020-06-04 16:31:45 +0100

    Finished Author routes

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const fs = require('fs');
 
 const Book = require('../models/book');
 const Author = require('../models/author');
